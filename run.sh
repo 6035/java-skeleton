96b8097086433dfa5a5fad8e57d3fb18e583c251
Wonsuk Choi <jchoi5me@mit.edu>
2019-09-08 10:58:41 -0400

    modified run.sh to accomodate for updated athena and java

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-gitroot=$(git rev-parse --show-toplevel)
-if [ -n "$JAVA_TOOL_OPTIONS" ]; then
-  java_tool_options="$JAVA_TOOL_OPTIONS"
-  unset JAVA_TOOL_OPTIONS
-fi
-java $java_tool_options -jar $gitroot/dist/Compiler.jar "$@"
+#!/usr/bin/env bash
+
+java -ea -jar 'dist/Compiler.jar' "$@"
